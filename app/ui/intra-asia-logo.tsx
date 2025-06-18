0e452899e44f2a52cf6730afd1b6cd702fbaf75b
Jenson <jenson0214@gmail.com>
2025-06-18 16:56:15 +0800

    - updated image height

--- app/ui/intra-asia-logo.tsx
+++ app/ui/intra-asia-logo.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 export default function IntraAsiaLogo() {
     return (
         <div
-            className={`${robotoCondensed.className} flex flex-row items-center leading-none text-white`}
-            >
-        <Image 
+            className={`${robotoCondensed.className} flex flex-row items-center leading-none text-white p-0 m-0`}
+        >
+        <Image
             src="/intraasia-logo.svg" 
             alt="Intraasia" 
             width={1838} 
             height={388}
-            className="w-[30vw] h-auto max-h-10 sm:max-h-12 md:max-h-14 min-w-0"
+            className="w-[30vw] h-auto max-h-[16vw] sm:max-h-[14vw] md:max-h-[18.8vw] min-w-0 p-0 m-0"
         />
         </div>
     )
